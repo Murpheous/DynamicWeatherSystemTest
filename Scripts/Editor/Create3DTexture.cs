1c13ab7fafb8d2ad148b553485d5dd79d4de17dd
iffn <iffn1990@gmail.com>
2023-11-24 14:45:43 +0100

    Initial commit

---
+++ Scripts/Editor/Create3DTexture.cs
@@ -0,0 +1,75 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace DynamicWeatherSystem.EditorScripts
+{
+    public class Texture3DTools : EditorWindow
+    {
+        [MenuItem("Tools/DynamicWeatherSystem/3D texture toolbox")]
+
+        public static void ShowWindow()
+        {
+            GetWindow(typeof(Texture3DTools));
+        }
+
+        Vector3Int size = new Vector3Int(100, 50, 100);
+        Color defaultColor = Color.black;
+
+        void OnGUI()
+        {
+            EditorGUILayout.LabelField("3D Texture tools:", EditorStyles.boldLabel);
+
+            size = EditorGUILayout.Vector3IntField("Size", size);
+            defaultColor = EditorGUILayout.ColorField("Default color", defaultColor);
+
+            if (size.x <= 0) size.x = 1;
+            if (size.y <= 0) size.y = 1;
+            if (size.z <= 0) size.z = 1;
+
+            if (GUILayout.Button(text: "Create 3D texture in Asset folder"))
+            {
+                CreateTexture3DInAssetFolder(size, defaultColor);
+            }
+        }
+
+        void CreateTexture3DInAssetFolder(Vector3Int size, Color defaultColor)
+        {
+            //Source: https://docs.unity3d.com/Manual/class-Texture3D.html
+
+            // Configure the texture
+
+            TextureFormat format = TextureFormat.RGBA32;
+            TextureWrapMode wrapMode = TextureWrapMode.Clamp;
+
+            // Create the texture and apply the configuration
+            Texture3D texture = new Texture3D(size.x, size.y, size.z, format, false);
+            texture.wrapMode = wrapMode;
+
+            // Create a 3-dimensional array to store color data
+            Color[] colors = new Color[size.x * size.y * size.z];
+
+            // Populate the array so that the x, y, and z values of the texture will map to red, blue, and green colors
+            for (int z = 0; z < size.z; z++)
+            {
+                int zOffset = z * size.x * size.y;
+                for (int y = 0; y < size.y; y++)
+                {
+                    int yOffset = y * size.x;
+                    for (int x = 0; x < size.x; x++)
+                    {
+                        colors[x + yOffset + zOffset] = defaultColor;
+                    }
+                }
+            }
+
+            // Copy the color values to the texture
+            texture.SetPixels(colors);
+
+            // Apply the changes to the texture and upload the updated texture to the GPU
+            texture.Apply();
+
+            // Save the texture to your Unity Project
+            AssetDatabase.CreateAsset(texture, $"Assets/3DTexture_{size.x}x{size.y}x{size.z}.asset");
+        }
+    }
+}
---
+++ Scripts/RuntimeTests/WeatherSystemTest.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DynamicWeatherSystem.RuntimeTests
+{
+    public class WeatherSystemTest : MonoBehaviour
+    {
+        //Basic definitions:
+        delegate void CellFunction(int x, int y, int z);
+
+        struct CellInformation
+        {
+            public float pressurePascal;
+            public float humidity;
+            public float temperatureKelvin;
+        }
+
+        //Basic parameters:
+        const float rockPressure = Mathf.Infinity;
+        const float waterHumidity = 2f;
+        const float oceanHumidity = Mathf.Infinity;
+
+        Color oceanColor = Color.blue;
+        Color clearColor = Color.black;
+
+        //Unity assignments:
+        [SerializeField] Texture3D linked3DTexture;
+
+        public float updateTimeMs = 0;
+
+        //Runtime data
+        CellInformation[,,] cellData;
+
+        //Unity functions:
+        void Start()
+        {
+            cellData = new CellInformation[linked3DTexture.width, linked3DTexture.height, linked3DTexture.depth];
+
+            RunFunctionForEachCell(SetupCellData);
+        }
+
+        void Update()
+        {
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+
+            RunFunctionForEachCell(IterateCellData);
+
+            WriteTextureData();
+
+            updateTimeMs = sw.ElapsedMilliseconds;
+        }
+
+        //Main functions:
+        void SetupCellData(int x, int y, int z)
+        {
+            int groundLevel = 5;
+
+            if (y < groundLevel) cellData[x, y, z].humidity = oceanHumidity;
+        }
+
+        void IterateCellData(int x, int y, int z)
+        {
+            CellInformation currentInformation = cellData[x, y, z];
+
+            //Cell modification logic goes here
+
+        }
+
+        Color32 GetColorOfCell(int x, int y, int z)
+        {
+            CellInformation currentInformation = cellData[x, y, z];
+
+            if (currentInformation.humidity == oceanHumidity) return oceanColor;
+            else return clearColor;
+        }
+
+        void WriteTextureData()
+        {
+            Vector3Int size = new Vector3Int(
+                cellData.GetLength(0),
+                cellData.GetLength(1),
+                cellData.GetLength(2));
+
+            Color32[] colors = new Color32[size.x * size.y * size.z];
+
+            for (int z = 0; z < size.z; z++)
+            {
+                int zOffset = z * size.x * size.y;
+                for (int y = 0; y < size.y; y++)
+                {
+                    int yOffset = y * size.x;
+                    for (int x = 0; x < size.x; x++)
+                    {
+                        colors[x + yOffset + zOffset] = GetColorOfCell(x, y, z);
+                    }
+                }
+            }
+
+            linked3DTexture.SetPixels32(colors); //Apparently using SetPixels32 is faster than SetPixels according to warning 
+
+            linked3DTexture.Apply();
+        }
+
+        void RunFunctionForEachCell(CellFunction cellFunction)
+        {
+            for (int x = 0; x < cellData.GetLength(0); x++)
+            {
+                for (int y = 0; y < cellData.GetLength(1); y++)
+                {
+                    for (int z = 0; z < cellData.GetLength(2); z++)
+                    {
+                        cellFunction(x, y, z);
+                    }
+                }
+            }
+        }
+
+        //Support functions:
+
+    }
+}
