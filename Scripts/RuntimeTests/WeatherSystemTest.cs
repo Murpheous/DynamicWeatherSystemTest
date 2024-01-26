02ea6be67e55213332c25ffac438db1b86c7c660
iffn <iffn1990@gmail.com>
2024-01-26 19:50:40 +0100

    Implemented basic diffusion formulas

--- Scripts/RuntimeTests/WeatherSystemTest.cs
+++ Scripts/RuntimeTests/WeatherSystemTest.cs
@@ -20,10 +20,13 @@ namespace DynamicWeatherSystem.RuntimeTests
         //Basic parameters:
         const float rockPressure = Mathf.Infinity;
         const float waterHumidity = 2f;
-        const float oceanHumidity = Mathf.Infinity;
+        const float oceanHumidity = 1f; //Mathf.Infinity;
 
         Color oceanColor = Color.blue;
         Color clearColor = Color.clear;
+        Vector3Int maxIndexes;
+
+        const float oneSixth = 1f / 6;
 
         //Unity assignments:
         [SerializeField] Texture3D linked3DTexture;
@@ -41,7 +44,9 @@ namespace DynamicWeatherSystem.RuntimeTests
 
             cellData = new CellInformation[gridSize.x, gridSize.y, gridSize.z];
 
-            RunFunctionForEachCell(SetupCellData);
+            maxIndexes = gridSize - Vector3Int.one;
+
+            RunFunctionForEachCell(SetupPointCellData);
         }
 
         void Update()
@@ -53,10 +58,14 @@ namespace DynamicWeatherSystem.RuntimeTests
             //Update cells: New data should not affect other cells
             CellInformation[,,]  newCellData = new CellInformation[gridSize.x, gridSize.y, gridSize.z];
 
+            totalTemperature = 0;
+
             RunFunctionForEachCell(delegate(int x, int y, int z)
             {
-                newCellData[x, y, z] = IterateCellDataTest1(x, y, z, cellData);
+                newCellData[x, y, z] = DiffusionTest(new Vector3Int(x, y, z), cellData, maxIndexes);
             });
+
+            averageTemperatureK = totalTemperature / (gridSize.x * gridSize.y * gridSize.z);
 
             cellData = newCellData;
 
@@ -65,20 +74,33 @@ namespace DynamicWeatherSystem.RuntimeTests
 
             updateTimeMs = sw.ElapsedMilliseconds;
         }
+        public float averageTemperatureK;
+        static float totalTemperature;
 
         //Main functions:
-        void SetupCellData(int x, int y, int z)
+        void SetupOceanFloorCellData(int x, int y, int z)
         {
-            int groundLevel = 5;
+            int groundLevel = 2;
 
             if (y < groundLevel) cellData[x, y, z].humidity = oceanHumidity;
         }
 
-        static CellInformation IterateCellDataTemplate(int x, int y, int z, CellInformation[,,] existingCellData)
+        void SetupSideWallCellData(int x, int y, int z)
+        {
+            int sideLevel = 20;
+
+            if (x < sideLevel) cellData[x, y, z].humidity = oceanHumidity;
+        }
+
+        void SetupPointCellData(int x, int y, int z)
+        {
+            if (x == 25 && y == 12 && z == 25) cellData[x, y, z].temperatureKelvin = 17000000;
+        }
+
+        static CellInformation IterateCellDataTemplate(Vector3Int coordinate, CellInformation[,,] existingCellData, Vector3Int maxIndexes)
         {
             //Cell modification logic goes here
-            CellInformation currentData = existingCellData[x, y, z];
-
+            CellInformation currentData = existingCellData[coordinate.x, coordinate.y, coordinate.z];
 
             return new CellInformation
             {
@@ -88,26 +110,79 @@ namespace DynamicWeatherSystem.RuntimeTests
             };
         }
 
-        static CellInformation IterateCellDataTest1(int x, int y, int z, CellInformation[,,] existingCellData)
+        static CellInformation DiffusionTest(Vector3Int coordinate, CellInformation[,,] existingCellData, Vector3Int maxIndexes)
         {
             //Cell modification logic goes here
-            CellInformation currentData = existingCellData[x, y, z];
+            CellInformation currentData = existingCellData[coordinate.x, coordinate.y, coordinate.z];
 
+            Vector3Int leftCellIndex = (coordinate.x != 0) ? coordinate + Vector3Int.left : coordinate;
+            Vector3Int rightIndex = (coordinate.x != maxIndexes.x) ? coordinate + Vector3Int.right : coordinate;
+            Vector3Int upIndex = (coordinate.y != maxIndexes.y) ? coordinate + Vector3Int.up : coordinate;
+            Vector3Int lowIndex = (coordinate.y != 0) ? coordinate + Vector3Int.down : coordinate;
+            Vector3Int forwardIndex = (coordinate.z != maxIndexes.z) ? coordinate + Vector3Int.forward : coordinate;
+            Vector3Int backIndex = (coordinate.z != 0) ? coordinate + Vector3Int.back : coordinate;
+
+            float averageSurroundingPressure = (
+                existingCellData[leftCellIndex.x, leftCellIndex.y, leftCellIndex.z].pressurePascal +
+                existingCellData[rightIndex.x, rightIndex.y, rightIndex.z].pressurePascal +
+                existingCellData[upIndex.x, upIndex.y, upIndex.z].pressurePascal +
+                existingCellData[lowIndex.x, lowIndex.y, lowIndex.z].pressurePascal +
+                existingCellData[forwardIndex.x, forwardIndex.y, forwardIndex.z].pressurePascal +
+                existingCellData[backIndex.x, backIndex.y, backIndex.z].pressurePascal
+                ) * oneSixth;
+
+            float averageSurroundingHumidity = (
+                existingCellData[leftCellIndex.x, leftCellIndex.y, leftCellIndex.z].humidity +
+                existingCellData[rightIndex.x, rightIndex.y, rightIndex.z].humidity +
+                existingCellData[upIndex.x, upIndex.y, upIndex.z].humidity +
+                existingCellData[lowIndex.x, lowIndex.y, lowIndex.z].humidity +
+                existingCellData[forwardIndex.x, forwardIndex.y, forwardIndex.z].humidity +
+                existingCellData[backIndex.x, backIndex.y, backIndex.z].humidity
+                ) * oneSixth;
+
+            float averageSurroundingTemperaturey = (
+                existingCellData[leftCellIndex.x, leftCellIndex.y, leftCellIndex.z].temperatureKelvin +
+                existingCellData[rightIndex.x, rightIndex.y, rightIndex.z].temperatureKelvin +
+                existingCellData[upIndex.x, upIndex.y, upIndex.z].temperatureKelvin +
+                existingCellData[lowIndex.x, lowIndex.y, lowIndex.z].temperatureKelvin +
+                existingCellData[forwardIndex.x, forwardIndex.y, forwardIndex.z].temperatureKelvin +
+                existingCellData[backIndex.x, backIndex.y, backIndex.z].temperatureKelvin
+                ) * oneSixth;
+
+            float newPressurePascal = currentData.pressurePascal + 0.1f * (averageSurroundingPressure - currentData.pressurePascal);
+            float newHumidity = currentData.humidity + 1f * (averageSurroundingHumidity - currentData.humidity);
+            float newTemperature = currentData.temperatureKelvin + 1f * (averageSurroundingTemperaturey - currentData.temperatureKelvin);
+
+            totalTemperature += newTemperature;
 
             return new CellInformation
             {
-                pressurePascal = currentData.pressurePascal,
-                humidity = currentData.humidity,
-                temperatureKelvin = currentData.temperatureKelvin
+                pressurePascal = newPressurePascal,
+                humidity = newHumidity,
+                temperatureKelvin = newTemperature
             };
         }
 
-        Color32 GetColorOfCell(int x, int y, int z)
+        Color32 GetVisualColorOfCell(int x, int y, int z)
         {
             CellInformation currentInformation = cellData[x, y, z];
 
             if (currentInformation.humidity == oceanHumidity) return oceanColor;
             else return clearColor;
+        }
+
+        Color32 GetDebugColorOfCell(int x, int y, int z)
+        {
+            CellInformation currentInformation = cellData[x, y, z];
+
+            Color32 returnColor = new Color32(
+                (byte)(Mathf.Clamp01((currentInformation.temperatureKelvin - 273.15f) / 100) * byte.MaxValue),
+                (byte)(Mathf.Clamp01(currentInformation.pressurePascal / 100000) * byte.MaxValue),
+                (byte)(Mathf.Clamp01(currentInformation.humidity) * byte.MaxValue),
+                byte.MaxValue
+                );
+
+            return returnColor;
         }
 
         //Support functions:
@@ -145,7 +220,7 @@ namespace DynamicWeatherSystem.RuntimeTests
                     int yOffset = y * size.x;
                     for (int x = 0; x < size.x; x++)
                     {
-                        colors[x + yOffset + zOffset] = GetColorOfCell(x, y, z);
+                        colors[x + yOffset + zOffset] = GetDebugColorOfCell(x, y, z);
                     }
                 }
             }
